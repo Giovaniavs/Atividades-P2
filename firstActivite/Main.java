52b26808e91070bbb32443301d207c6e407d8798
Giovaniavs <gavs2@cin.ufpe.br>
2021-10-29 18:40:44 -0300

    mudando import da atividade 1

---
+++ firstActivite/Main.java
@@ -0,0 +1,39 @@
+package firstActivite;
+
+public class Main {
+
+  public static void main(String[] args) {
+    System.out.println("\n ========== Primeira Operação ========== \n");
+
+    Point p = new Point(10, -10); // Vai emitir mensagem de erro no Y
+
+    System.out.println("\n ========== Segunda Operação ========== \n");
+
+    Point p1 = new Point(10, 10); // Sucesso!
+
+    System.out.println("\n ========== Terceira Operação ========== \n");
+
+    Point p2 = new Point(500, 10); // Vai emitir mensagem de erro no X
+
+    System.out.println("\n ========== Quarta Operação ========== \n");
+
+
+    p.setX(-100); // Vai emitir mensagem de erro no X
+    p.setX(500); // Vai emitir mensagem de erro no X
+    p.setX(300); // Sucesso!
+
+    System.out.println("\n ========== Quinta Operação ========== \n");
+
+    p.setY(-100); // Vai emitir mensagem de erro no Y
+    p.setY(500); // Vai emitir mensagem de erro no Y
+    p.setY(300); // Sucesso!
+
+    System.out.println("\n ========== Sexta Operação ========== \n");
+
+    p.moveBy(100, 100); // Vai emitir dois Sucessos!
+    p.moveBy(200, 100); // Vai emitir erro no X e sucesso no Y
+    p.moveBy(100, -400); // Vai emitir erro no Y e sucesso no X
+
+  }
+  
+}
---
+++ firstActivite/Point.java
@@ -0,0 +1,47 @@
+package firstActivite;
+
+public class Point {
+    private int x = 0, y = 0;
+
+    public Point(int horizontalX, int verticalY) {
+        if (horizontalX >=0 && horizontalX <= 400) {
+            this.x = horizontalX;
+            System.out.println("O valor de X é válido!");
+        } else {
+            System.out.println("O valor de X é inválido...");
+        }
+
+        if (verticalY >=0 && verticalY <= 400) {
+            this.y = verticalY;
+            System.out.println("O valor de Y é válido!");
+        } else {
+            System.out.println("O valor de Y é inválido...");
+        }
+    }
+
+    public int getX() { return x; }
+    public int getY() { return y; }
+
+    public void setX(int x) { 
+        if (x >=0 && x <= 400) {
+            this.x = x; 
+            System.out.println("Sucesso!");
+        } else {
+            System.out.println("O valor de X só pode estar entre 0 e 400");
+        }
+    }
+
+    public void setY(int y) { 
+        if (y >=0 && y <= 400) {
+            this.y = y; 
+            System.out.println("Sucesso!");
+        } else {
+            System.out.println("O valor de Y só pode estar entre 0 e 400");
+        } 
+    }
+
+    public void moveBy(int dx, int dy) {
+        setX(getX() + dx);
+        setY(getY() + dy);
+    }
+}
